b78240b8d380d52667d0cd8bbf391aec41cc4c1e
Alexey Korotin <sx12fx@gmail.com>
2023-12-04 23:46:50 +0300

    fix: comment mapper

--- backend/src/main/java/com/em/testtask/mapper/CommentMapper.java
+++ backend/src/main/java/com/em/testtask/mapper/CommentMapper.java
@@ -1,8 +1,8 @@
 package com.em.testtask.mapper;
 
 import com.em.testtask.domain.Comment;
-import com.em.testtask.dto.InputCommentDto;
-import com.em.testtask.dto.OutputCommentDto;
+import com.em.testtask.dto.domain.InputCommentDto;
+import com.em.testtask.dto.domain.OutputCommentDto;
 import com.em.testtask.mapper.config.MapConfig;
 import org.mapstruct.Mapper;
 
