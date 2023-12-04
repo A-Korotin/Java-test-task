c683e2e1022f212038c49f190f387ef837103ddf
Alexey Korotin <sx12fx@gmail.com>
2023-12-04 17:51:23 +0300

    feat: init model

---
+++ backend/src/main/java/com/em/testtask/domain/Comment.java
@@ -0,0 +1,25 @@
+package com.em.testtask.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "comments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+    @Id
+    private UUID id;
+
+    @ManyToOne
+    private Task task;
+
+    private String comment;
+
+    private String authorId;
+}
---
+++ backend/src/main/java/com/em/testtask/domain/TaskPriority.java
@@ -0,0 +1,7 @@
+package com.em.testtask.domain;
+
+public enum TaskPriority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
