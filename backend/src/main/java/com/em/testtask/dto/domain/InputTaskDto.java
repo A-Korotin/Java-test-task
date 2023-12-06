0c3e958b3f46cc5498e3b1e318dd1e98a42e5de4
Alexey Korotin <sx12fx@gmail.com>
2023-12-06 17:29:49 +0300

    test: add task mvc tests

--- backend/src/main/java/com/em/testtask/dto/domain/InputTaskDto.java
+++ backend/src/main/java/com/em/testtask/dto/domain/InputTaskDto.java
@@ -4,9 +4,11 @@ import com.em.testtask.domain.TaskPriority;
 import com.em.testtask.domain.TaskStatus;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class InputTaskDto {
     @NotNull
     @NotEmpty
