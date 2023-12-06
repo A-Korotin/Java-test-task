88208b382701e39d0d5df4745c1cf34c6b86b099
Alexey Korotin <sx12fx@gmail.com>
2023-12-06 16:39:39 +0300

    feat: verbose exception handling

--- backend/src/main/java/com/em/testtask/dto/error/ErrorDto.java
+++ backend/src/main/java/com/em/testtask/dto/error/ErrorDto.java
@@ -1,12 +1,22 @@
 package com.em.testtask.dto.error;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ErrorDto {
     public String message;
+
+    @Builder.Default
+    public Map<String, String> errors = Collections.emptyMap();
+
 }
