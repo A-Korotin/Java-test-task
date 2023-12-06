58636d68c246115348d6dbf4500d538b565ac514
Alexey Korotin <sx12fx@gmail.com>
2023-12-06 16:17:24 +0300

    docs: improve swagger documentation

--- backend/src/main/java/com/em/testtask/controller/CommentController.java
+++ backend/src/main/java/com/em/testtask/controller/CommentController.java
@@ -4,22 +4,31 @@ import com.em.testtask.domain.Comment;
 import com.em.testtask.domain.Task;
 import com.em.testtask.dto.domain.InputCommentDto;
 import com.em.testtask.dto.domain.OutputCommentDto;
+import com.em.testtask.dto.error.ErrorDto;
 import com.em.testtask.exception.NotFoundException;
 import com.em.testtask.mapper.CommentMapper;
 import com.em.testtask.service.CommentService;
 import com.em.testtask.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.UUID;
 
 @RestController
+@Validated
 @RequestMapping("/tasks/{taskId}/comments")
 @RequiredArgsConstructor
+@Tag(name = "Task comments", description = "Manage comments")
 public class CommentController {
     private final CommentService commentService;
     private final TaskService taskService;
@@ -27,6 +36,12 @@ public class CommentController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create comment",
+            description = "Create comment and associate it to the task",
+            responses = {@ApiResponse(description = "Created", responseCode = "201", useReturnTypeSchema = true),
+                    @ApiResponse(description = "Invalid request body/path variables", responseCode = "400", content = {@Content(schema = @Schema(implementation = ErrorDto.class))}),
+                    @ApiResponse(description = "Missing/invalid access token", responseCode = "401", content = {@Content(schema = @Schema(implementation = Void.class))}),
+                    @ApiResponse(description = "Not found (more details in response body)", responseCode = "404", content = {@Content(schema = @Schema(implementation = ErrorDto.class))})})
     public OutputCommentDto createComment(@PathVariable UUID taskId,
                                           @Valid @RequestBody InputCommentDto dto,
                                           Principal currentUser) {
@@ -42,6 +57,13 @@ public class CommentController {
     }
 
     @PutMapping("/{commentId}")
+    @Operation(summary = "Update comment",
+            description = "Update comment contents",
+            responses = {@ApiResponse(description = "Ok", responseCode = "200", useReturnTypeSchema = true),
+                    @ApiResponse(description = "Invalid request body/path variables", responseCode = "400", content = {@Content(schema = @Schema(implementation = ErrorDto.class))}),
+                    @ApiResponse(description = "Missing/invalid access token", responseCode = "401", content = {@Content(schema = @Schema(implementation = Void.class))}),
+                    @ApiResponse(description = "No access", responseCode = "403", content = {@Content(schema = @Schema(implementation = Void.class))}),
+                    @ApiResponse(description = "Not found (more details in response body)", responseCode = "404", content = {@Content(schema = @Schema(implementation = ErrorDto.class))})})
     @PreAuthorize("@commentSecurityService.userHasRightsToModify(#commentId, principal)")
     public OutputCommentDto editComment(@PathVariable UUID taskId,
                                         @PathVariable UUID commentId,
@@ -56,6 +78,13 @@ public class CommentController {
 
     @DeleteMapping("/{commentId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete comment",
+            description = "Delete comment",
+            responses = {@ApiResponse(description = "Deleted", responseCode = "204", useReturnTypeSchema = true),
+                    @ApiResponse(description = "Invalid path variables", responseCode = "400", content = {@Content(schema = @Schema(implementation = ErrorDto.class))}),
+                    @ApiResponse(description = "Missing/invalid access token", responseCode = "401", content = {@Content(schema = @Schema(implementation = Void.class))}),
+                    @ApiResponse(description = "No access", responseCode = "403", content = {@Content(schema = @Schema(implementation = Void.class))}),
+                    @ApiResponse(description = "Not found (more details in response body)", responseCode = "404", content = {@Content(schema = @Schema(implementation = ErrorDto.class))})})
     @PreAuthorize("@commentSecurityService.userHasRightsToModify(#commentId, principal)")
     public void deleteById(@PathVariable UUID taskId, @PathVariable UUID commentId) {
         Task parentTask = taskService.findById(taskId).orElseThrow(() ->
